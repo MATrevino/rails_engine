09f3d926e58e86bf5921ca370484beaa109ab709
Meredith Trevino <meredithtrevino3@gmail.com>
2023-03-27 16:02:49 -0600

    adds show and index tests for merchant

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :customers, only: [:index]
       resources :invoices, only: [:index]
+      resources :merchants, only: [:index, :show]
     end
   end
 end
