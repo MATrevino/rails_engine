7ff97b05dfa54cfac0f9f8eb90cd25d2bd95bb94
Meredith Trevino <meredithtrevino3@gmail.com>
2023-03-28 12:46:41 -0600

    adds updated serializers with gem, updates merchant_request_spec tests

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,30 +1,34 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.all
-    render json: ItemSerializer.format_items(items)
+    # items = Item.all
+    # render json: ItemSerializer.format_items(items)
+    render json: ItemSerializer.new(Item.all)
   end
 
   def show
-    item = Item.find(params[:id])
-    render json: ItemSerializer.format_item_show(item)
+    # item = Item.find(params[:id])
+    # render json: ItemSerializer.format_item_show(item)
+    render json: ItemSerializer.new(Item.find(params[:id]))
   end
 
   def create
-    # merchant = Merchant.find(item_params[:merchant_id])
     Item.create(item_params)
-    render json: ItemSerializer.format_item_create(Item.last)
+    # render json: ItemSerializer.format_item_create(Item.last)
+    render json: ItemSerializer.new(Item.last)
   end
 
   def update
     item = Item.find(params[:id])
     item.update(item_params)
-    render json: ItemSerializer.format_item_create(item)
+    # render json: ItemSerializer.format_item_create(item)
+    render json: ItemSerializer.new(item)
   end
 
   def destroy
     item = Item.find(params[:id])
     item.destroy
     render json: ItemSerializer.format_item_create(item)
+ 
   end
 
   private
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,30 +1,4 @@
 class ItemSerializer
-  def self.format_items(items)
-    items.map do |item|
-      {
-        id: item.id,
-        name: item.name,
-        unit_price: item.unit_price,
-        description: item.description
-      }
-    end
-  end
-
-  def self.format_item_show(item)
-    {
-      id: item.id,
-      name: item.name,
-      unit_price: item.unit_price,
-      description: item.description
-    }
-  end
-
-  def self.format_item_create(item)
-    {
-      id: item.id,
-      name: item.name,
-      unit_price: item.unit_price,
-      description: item.description
-    }
-  end
+  include JSONAPI::Serializer
+  attributes :name, :description, :unit_price, :merchant_id
 end
